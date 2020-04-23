93034b91791441dcb600a69d4c6ea79e08aba1ad
Natawat Kwanpoom <natawat.nueng@gmail.com>
2020-04-23 18:08:48 +0700

    :tada: Init project

---
+++ server.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return 'Hello World!';
+        }
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/part1',
+        handler: (request, h) => {
+            
+            return request.payload;;
+        }
+    });
+
+
+    server.route({
+        method: '*',
+        path: '/{any*}',
+        handler: function (request, h) {
+
+            return '404 Error! Page Not Found!';
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
